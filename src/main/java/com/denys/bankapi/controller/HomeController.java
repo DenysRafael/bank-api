9a8a4b9ffab14a202fb3e58bf314d08d350fb007
Rafael <rafaelsantosvideos6@gmail.com>
2026-04-30 14:50:52 -0300

    Configurando rota principal e application.properties

---
+++ src/main/java/com/denys/bankapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.denys.bankapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bank API do Denys está online! Use os endpoints de /api/accounts para interagir.";
+    }
+}
